96511089c2a7cbf89c7350319e27a239f2582789
Albin <albwa833@student.liu.se>
2020-08-23 15:54:15 +0200

    Delete and move buttons functional

--- front.py
+++ front.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import os
 import back
 
+DEBUG = True
+
 class Windows(tk.Tk):
     """Backend for window management."""
 
@@ -48,7 +50,7 @@ class ComparePathsWindow(tk.Frame):
         self.right_path = tk.Entry(paths, width=entry_width)
         # REMOVE, temporary for testing
         self.left_path.insert(0, "D:\Documents\Ämnen")
-        self.right_path.insert(0, "D:\Documents\Ämnen (old)")
+        self.right_path.insert(0, "D:\Documents\Gymnasiet")
         self.left_path.grid(row=1, column=0)
         self.right_path.grid(row=1, column=2)
         tk.Button(paths, text="Swap", command=self.swap) \
@@ -64,70 +66,6 @@ class ComparePathsWindow(tk.Frame):
         path = file.full_name
         def func(path=path):
             os.startfile(path)
-        return func
-
-    def add_to_delete(self, files, id):
-        """Mark a file for deletion, wrapper.
-
-        If the file already is in the list, remove it from the list.
-        The buttons relief will also change as an indicator of if it has been
-        pressed.
-        """
-        def func(self=self, files=files, id=id):
-            button = self.delete_buttons[id]
-            if isinstance(files, back.File):
-                files = [files]
-            if button["relief"] == "sunken":
-                button["relief"] = "raised"
-                for file in files:
-                    i = self.to_delete.index(file)
-                    del self.to_delete[i]
-            else:
-                self.delete_buttons[id]["relief"] = "sunken"
-                for file in files:
-                    self.to_delete.append(file)
-            print(self.to_delete)
-        return func
-
-    def keep(self, files, id, type_):
-
-        def func(self=self, files=files, id=id, type_=type_):
-            button = self.keep_buttons[id][type_]
-            # Check if another button is clicked, only one can be active per
-            # file.
-            if type_ == "main":
-                other_type = "comp"
-            else:
-                other_type = "main"
-            other_button = self.keep_buttons[id][other_type]
-            if other_button["relief"] == "sunken":
-                # Button must be unclicked
-                click(self, other_button, files, other_type)
-            # Click this button
-            click(self, button, files, type_)
-            print(self.to_move)
-
-        def click(self, button, files, type_):
-            print("B", self.to_delete)
-            main, comp = files
-            if button["relief"] == "raised":
-                # The button has not been pressed before
-                button["relief"] = "sunken"
-                if type_ == "main":
-                    # Delete comp
-                    self.to_delete.append(comp)
-                elif type_ == "comp":
-                    # Move comp to main path
-                    self.to_move.append(comp)
-            elif button["relief"] == "sunken":
-                button["relief"] = "raised"
-                if type_ == "main":
-                    i = self.to_delete.index(comp)
-                    del self.to_delete[i]
-                elif type_ == "comp":
-                    i = self.to_move.index(comp)
-                    del self.to_move[i]
-
         return func
 
     def load(self):
@@ -150,6 +88,7 @@ class ComparePathsWindow(tk.Frame):
             pass
         self.bottom = tk.Frame(self)
         indent = (10, 0)
+        width = 10
         self.delete_buttons = {}
         self.to_delete = []
         self.keep_buttons = {}
@@ -165,9 +104,13 @@ class ComparePathsWindow(tk.Frame):
                       font=self.controller.small_font, pady=0,
                       command=self.open_file(file)) \
                       .grid(row=i, column=2, sticky="we")
+            self.keep_buttons[i] = tk.Button(self.bottom, text="Move",
+                                             width=width,
+                                             command=self.keep(file, i))
+            self.keep_buttons[i].grid(row=i, column=4, sticky="we", padx=indent)
             self.delete_buttons[i] = tk.Button(self.bottom, text="Delete",
                                                command=self.add_to_delete(file, i))
-            self.delete_buttons[i].grid(row=i, column=4, sticky="we")
+            self.delete_buttons[i].grid(row=i, column=5, sticky="we")
         # Changed files
         tk.Label(self.bottom, text="Changed files:",
                  font=self.controller.large_font) \
@@ -188,15 +131,110 @@ class ComparePathsWindow(tk.Frame):
             self.keep_buttons[j]["main"] = tk.Button(self.bottom,
                                                      text="Keep main",
                                                      command=self.keep(files, j, "main"))
-            self.keep_buttons[j]["main"].grid(row=j, column=4)
+            self.keep_buttons[j]["main"].grid(row=j, column=4, padx=indent, sticky="we")
             self.keep_buttons[j]["comp"] = tk.Button(self.bottom, text="Keep other",
+
                                                      command=self.keep(files, j, "comp"))
-            self.keep_buttons[j]["comp"].grid(row=j, column=5)
+            self.keep_buttons[j]["comp"].grid(row=j, column=5, sticky="we")
             self.delete_buttons[j] = tk.Button(self.bottom, text="Delete both",
                                                command=self.add_to_delete(files, j))
-            self.delete_buttons[j].grid(row=j, column=6)
+            self.delete_buttons[j].grid(row=j, column=6, sticky="we")
         print(self.delete_buttons)
         self.bottom.pack(fill="both")
+
+    def add_to_delete(self, files, id, check_row=True):
+        """Mark a file for deletion, wrapper.
+
+        If the file already is in the list, remove it from the list.
+        The buttons relief will also change as an indicator of if it has been
+        pressed.
+        """
+        def func(self=self, files=files, id=id, check_row=check_row):
+            if check_row:
+                # Check if any other button with same id is clicked.
+                keep = self.keep_buttons[id]
+                if isinstance(keep, dict):
+                    for type_str in ("main", "comp"):
+                        button = keep[type_str]
+                        if button["relief"] == "sunken":
+                            self.keep(files, id, type_str)()
+                else:
+                    if keep["relief"] == "sunken":
+                        self.keep(files, id)()
+            # Click this button
+            button = self.delete_buttons[id]
+            if isinstance(files, back.File):
+                files = [files]
+            if button["relief"] == "sunken":
+                button["relief"] = "raised"
+                for file in files:
+                    i = self.to_delete.index(file)
+                    del self.to_delete[i]
+            else:
+                self.delete_buttons[id]["relief"] = "sunken"
+                for file in files:
+                    self.to_delete.append(file)
+            if DEBUG:
+                print("\nM", self.to_move)
+                print("D", self.to_delete)
+        return func
+
+    def keep(self, files, id, type_=None):
+        """Keep this file and remove alternetives, wrapper.
+
+        Arguments:
+        * files: A back.File instance, or a list of two
+        * id: A number uniqe for every row
+        * type_: None, 'main' or 'comp'. Is the file that this button
+                 represents from main or comp. None if only one version
+                 avalible (comp)."""
+
+        def func(self=self, files=files, id=id, type_=type_):
+            if type_ is None:
+                button = self.keep_buttons[id]
+            else:
+                if type_ == "main":
+                    other_type = "comp"
+                elif type_ == "comp":
+                    other_type = "main"
+                button = self.keep_buttons[id][type_]
+                other_button = self.keep_buttons[id][other_type]
+                if other_button["relief"] == "sunken":
+                    # Button must be unclicked
+                    click(self, other_button, files, other_type)
+            if self.delete_buttons[id]["relief"] == "sunken":
+                # Button must be unclicked
+                self.add_to_delete(files, id, check_row=False)()
+            # Click this button
+            click(self, button, files, type_)
+            if DEBUG:
+                print("\nM", self.to_move)
+                print("D", self.to_delete)
+
+        def click(self, button, files, type_):
+            if type_ is None:
+                comp = files
+            else:
+                main, comp = files
+            if button["relief"] == "raised":
+                # The button has not been pressed before
+                button["relief"] = "sunken"
+                if type_ == "main":
+                    # Delete comp
+                    self.to_delete.append(comp)
+                elif type_ == "comp" or type_ is None:
+                    # Move comp to main path
+                    self.to_move.append(comp)
+            elif button["relief"] == "sunken":
+                button["relief"] = "raised"
+                if type_ == "main":
+                    i = self.to_delete.index(comp)
+                    del self.to_delete[i]
+                elif type_ == "comp" or type_ is None:
+                    i = self.to_move.index(comp)
+                    del self.to_move[i]
+
+        return func
 
     def swap(self):
         """Swap the text of the two entrys."""
@@ -207,10 +245,6 @@ class ComparePathsWindow(tk.Frame):
         self.right_path.delete(0, "end")
         self.left_path.insert(0, old_right)
         self.right_path.insert(0, old_left)
-
-
-
-
 
 
 if __name__ == "__main__":
